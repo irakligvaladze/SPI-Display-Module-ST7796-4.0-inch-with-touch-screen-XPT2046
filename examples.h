1661da1d1caf8ba467d5201aa0db97eee7397693
Irakli Gvaladze <ikagvd@gmail.com>
2025-10-19 19:00:44 -0400

    Update examples.h

--- examples.h
+++ examples.h
@@ -10,9 +10,9 @@ void Test_Circle(void);
 void Test_Triangle(void);
 void English_Font_test(void);
 void Pic_test(void);
-void Load_Drow_Dialog(void);
 void Touch_Test(void);
 void main_test(void);
 void Rotate_Test(void);
 
 #endif
+
